5e3b2680c7810de3416da90abf9f43e49a4b03fb
jnosky <codegrinder69@hotmail.com>
2011-11-25 21:37:43 -0500

    Initial commit of IO_Toggle project

---
+++ IO_Toggle/main.c
@@ -0,0 +1,127 @@
+/**
+  ******************************************************************************
+  * @file    IO_Toggle/main.c 
+  * @author  MCD Application Team
+  * @version V1.0.0
+  * @date    19-September-2011
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************  
+  */ 
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_conf.h"
+#include "discoveryf4utils.h"
+
+/** @addtogroup STM32F4_Discovery_Peripheral_Examples
+  * @{
+  */
+
+/** @addtogroup IO_Toggle
+  * @{
+  */ 
+
+/* Private typedef -----------------------------------------------------------*/
+GPIO_InitTypeDef  GPIO_InitStructure;
+
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+void Delay(__IO uint32_t nCount);
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+	/*!< At this stage the microcontroller clock setting is already configured,
+       this is done through SystemInit() function which is called from startup
+       file (startup_stm32f4xx.s) before to branch to application main.
+       To reconfigure the default setting of SystemInit() function, refer to
+        system_stm32f4xx.c file
+    */
+
+	STM_EVAL_LEDInit(LED_RED);
+	STM_EVAL_LEDInit(LED_GREEN);
+	STM_EVAL_LEDInit(LED_BLUE);
+	STM_EVAL_LEDInit(LED_ORANGE);
+
+	while (1)  {
+
+		STM_EVAL_LEDOn(LED_ORANGE);
+    
+		Delay(0x3FFFFF);
+    
+		STM_EVAL_LEDOn(LED_RED);
+    
+		Delay(0x3FFFFF);
+  
+		STM_EVAL_LEDOn(LED_BLUE);
+    
+		Delay(0x3FFFFF);
+    
+		STM_EVAL_LEDOn(LED_GREEN);
+    
+		Delay(0x7FFFFF);
+
+		GPIO_ResetBits(GPIOD, LED_RED_PIN|LED_GREEN_PIN|LED_BLUE_PIN|LED_ORANGE_PIN);
+    
+		Delay(0xFFFFFF);
+	}
+}
+
+/**
+  * @brief  Delay Function.
+  * @param  nCount:specifies the Delay time length.
+  * @retval None
+  */
+void Delay(__IO uint32_t nCount)
+{
+  while(nCount--)
+  {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
